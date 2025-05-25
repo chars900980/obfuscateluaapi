52151f67b642508aa962d5172dc7f55e88190d52
unknown <liminhomcc@gmail.com>
2025-05-25 13:19:27 +0700

    sigma

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
